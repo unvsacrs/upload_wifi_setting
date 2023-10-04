0a247a646c72b0b478b79b48d486e065595c5f6b
unvsacrs <toa16659_n0zak1@au.com>
2023-10-04 13:36:39 +0900

    エラーハンドリングをmatchを使わずに実装

--- src/main.rs
+++ src/main.rs
@@ -4,62 +4,68 @@ use std::io::{Read};
 use std::time::Duration;
 use std::{thread};
 
-fn select_serialport() -> Option<String> {
-    match serialport::available_ports() {
-        Ok(ports) => {
-            if ports.len() < 1 {
-                return None
-            }
-            
-            for (index, port) in ports.iter().enumerate() {
-                println!("{}: {}", index + 1, port.port_name);
-            }
-            println!("シリアルポートを選択してください(番号を選んてEnterキーを押す)");
-        
-            let mut word = String::new();
-            std::io::stdin().read_line(&mut word).ok();
-            match word.trim().to_string().parse::<usize>() {
-                Ok(index) => {
-                    if index < 1 || index > ports.len() {
-                        return None
-                    }
-                    Some(ports[index - 1].port_name.clone())
-                },
-                Err(_) => None
-            }
-        },
-        Err(_) => {
-            None
-        }
+fn select_serialport() -> Result<String, String> {
+    let ports = serialport::available_ports().map_err(|_e| {"ポートが見つかりません"})?;
+    if ports.len() < 1 {
+        return Err("ポートが見つかりません".to_string());
     }
+    for (index, port) in ports.iter().enumerate() {
+        println!("{}: {}", index + 1, port.port_name);
+    }
+    println!("シリアルポートを選択してください(番号を選んてEnterキーを押す)");
+
+    let mut word = String::new();
+    std::io::stdin().read_line(&mut word).ok();
+
+    let index = word.trim().to_string().parse::<usize>().map_err(|_e| {"不正なインデックスです"})?;
+    if index < 1 || index > ports.len() {
+        return Err("不正なインデックスです".to_string());
+    }
+
+    Ok(ports[index - 1].port_name.clone())
 }
 
-fn write_data(name: String, data: &Vec<u8>) -> bool {
-    match serialport::new(name, 115200)
-    .stop_bits(serialport::StopBits::One)
-    .data_bits(serialport::DataBits::Eight)
-    .parity(serialport::Parity::None)
-    .timeout(Duration::from_millis(10000)).open() {
-        Ok(mut port) => {
-            let start = 2;
-            port.write(&[start]).expect("ポートに書き込めません");
-            thread::sleep(Duration::from_millis(500));
-    
-            for &byte in data.iter() {
-                port.write(&[byte]).expect("ポートに書き込めません");
-                thread::sleep(Duration::from_millis(10));
-            }
 
-            thread::sleep(Duration::from_millis(500));
-            let end = 3;
-            port.write(&[end]).expect("ポートに書き込めません");
-        },
-        Err(_) => {
-            println!("ポートを開けません");
-            return false;
-        }
+fn write_data(name: String, data: &Vec<u8>) -> Result<(), Box<dyn std::error::Error>> {
+    let mut port = serialport::new(name, 115200)
+        .stop_bits(serialport::StopBits::One)
+        .data_bits(serialport::DataBits::Eight)
+        .parity(serialport::Parity::None)
+        .timeout(Duration::from_millis(10000))
+        .open().map_err(|e| {format!("ポートを開けません({error})", error = e.to_string())})?;
+    
+    let start = 2;
+    port.write(&[start]).map_err(|e| {format!("ポートに書き込めません({error})", error = e.to_string())})?;
+    thread::sleep(Duration::from_millis(500));
+
+    for &byte in data.iter() {
+        port.write(&[byte]).map_err(|e| {format!("ポートに書き込めません({error})", error = e.to_string())})?;
+        thread::sleep(Duration::from_millis(10));
     }
-    true
+
+    thread::sleep(Duration::from_millis(500));
+    let end = 3;
+    port.write(&[end]).map_err(|e| {format!("ポートに書き込めません({error})", error = e.to_string())})?;
+
+    Ok(())
+}
+
+fn process_file_and_serial(filepath: String) -> Result<(), Box<dyn std::error::Error>> {
+    let portname = select_serialport().map_err(|e| {format!("Error: 正しいシリアルポートを選択してください({error})", error = e.to_string())})?;
+    println!("serialport: {}", portname);
+
+    let mut buffer = Vec::new();
+    let mut file = File::open(filepath).map_err(|e| {format!("Error: ファイルが開けませんでした({error})", error = e.to_string())})?;
+
+    file.read_to_end(&mut buffer).map_err(|e| {format!("Error: ファイルが読み込めませんでした({error})", error = e.to_string())})?;
+    
+    println!("{}", String::from_utf8(buffer.clone()).unwrap());
+
+    write_data(portname, &buffer).map_err(|e| {format!("Error: 設定送信に失敗しました({error})", error = e.to_string())})?;
+
+    println!("設定送信完了しました");
+
+    Ok(())
 }
 
 fn main() {
@@ -72,39 +78,8 @@ fn main() {
     println!("filename: {}", args[1]);
     let ref filepath = args[1];
 
-    match select_serialport() {
-        Some(portanme) => {
-            println!("serialport: {}", portanme);
-        
-            match File::open(filepath) {
-                Ok(mut file) => {
-                    let mut buffer = Vec::new();
-                    match file.read_to_end(&mut buffer) {
-                        Ok(_) => {
-                            println!("{}", String::from_utf8(buffer.clone()).unwrap());
-                            if write_data(portanme, &buffer) {
-                                println!("設定送信完了しました");
-
-                            } else {
-                                println!("設定送信に失敗しました");
-
-                            }
-                        },
-                        Err(_) => {
-                            println!("ファイルが読み込めませんでした");
-
-                        }
-                    }
-                }
-                Err(_) => {
-                    println!("ファイルが開けませんでした");
-
-                }
-            }
-        },
-        None => {
-            println!("正しいシリアルポートを選択してください");
-        }
+    if let Err(e) = process_file_and_serial(filepath.to_string()) {
+        println!("{}", e);
     }
 }
 
